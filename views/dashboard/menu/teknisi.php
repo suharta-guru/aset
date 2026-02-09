a8a4c07d3247102c24fe87bfb8c113527f626c50
suharta-guru <suharta.guru@gmail.com>
2026-02-09 10:06:15 +0700

    menambahkan menu dan isi pembaruan atau maintenech

--- views/dashboard/menu/teknisi.php
+++ views/dashboard/menu/teknisi.php
@@ -1,4 +1,12 @@
+<?php
+    switch($pg)
+    {
+        case'kbaru':$active_kbaru = 'active';break;
+            
+    }
+?>
+
 <div class="nav-section-label">Teknisi Aset</div>
-<a href="#" class="nav-link-custom">
+<a href="?pg=kbaru&fl=list" class="nav-link-custom <?= $active_kbaru ?>">
     <i class="bi bi-wrench"></i> Perbaruan
 </a>
